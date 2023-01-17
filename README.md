13e22e14e89b58b84a51435e7c254eec984ee0b6
Gladi023 <Gladimar.Villamil@gmail.com>
2023-01-16 18:01:17 -0800

    css and readme updated

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # WorkDay_Scheduler
 A daily Planner
+README Items:
+Installation
+Usage
+Credits
+License
+
+
+## Installation
+Deploy the website and navigate through the website.
+
+## Usage
+The website can be deployed by clicking on the following link:
+https://gladi023.github.io/WorkDay_Scheduler/
+
+After deploying the website the user can check the weather bu searching by city:
+![Screenshot of the website](./assets/images/Screen%20Shot%202023-01-16%20at%205.58.02%20PM.png)
+
+## Credits
+The original material that is included in the referenced websitres reviewed belongs to Trilogy Education Services. 
+
+## License
+MIT License
+
+Copyright (c) [2022] [Gladimar]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,62 +1,63 @@
-const addBtn = document.getElementById('add')
+const addBtn = document.getElementById('add');
 
-const notes = JSON.parse(localStorage.getItem('notes'))
+const notes = JSON.parse(localStorage.getItem('notes'));
 
-if(notes) {
-    notes.forEach(note => addNewNote(note))
+if (notes) {
+    notes.forEach(note => addNewNote(note));
 }
 
-addBtn.addEventListener('click', () => addNewNote())
+addBtn.addEventListener('click', addNote);
 
 function addNewNote(text = '') {
-    const note = document.createElement('div')
-    note.classList.add('note')
+    const note = document.createElement('div');
+    note.classList.add('note');
 
-    note.innerHTML = `
-    <div class="tools">
-        <button class="edit"><i class="fas fa-edit"></i></button>
-        <button class="delete"><i class="fas fa-trash-alt"></i></button>
-    </div>
-    <div class="main ${text ? "" : "hidden"}"></div>
-    <textarea class="${text ? "hidden" : ""}"></textarea>
-    `
+    const noteInnerHTML = `
+        <div class="tools">
+            <button class="edit"><i class="fas fa-edit"></i></button>
+            <button class="delete"><i class="fas fa-trash-alt"></i></button>
+        </div>
+        <div class="main ${text ? "" : "hidden"}"></div>
+        <textarea class="${text ? "hidden" : ""}"></textarea>
+    `;
 
-    const editBtn = note.querySelector('.edit')
-    const deleteBtn = note.querySelector('.delete')
-    const main = note.querySelector('.main')
-    const textArea = note.querySelector('textarea')
+    note.innerHTML = noteInnerHTML;
 
-    textArea.value = text
-    main.innerHTML = marked(text)
+    const editBtn = note.querySelector('.edit');
+    const deleteBtn = note.querySelector('.delete');
+    const main = note.querySelector('.main');
+    const textArea = note.querySelector('textarea');
+
+    textArea.value = text;
+    main.innerHTML = marked(text);
 
     deleteBtn.addEventListener('click', () => {
-        note.remove()
-
-        updateLS()
-    })
-
-    editBtn.addEventListener('click', () => {
-        main.classList.toggle('hidden')
-        textArea.classList.toggle('hidden')
-    })
-
+      note.remove();
+      updateLS();
+      });
+      editBtn.addEventListener('click', () => {
+        main.classList.toggle('hidden');
+        textArea.classList.toggle('hidden');
+    });
+    
     textArea.addEventListener('input', (e) => {
-        const { value } = e.target
+        const { value } = e.target;
+        main.innerHTML = marked(value);
+        updateLS();
+    });
+    
+    document.body.appendChild(note);
+  }
 
-        main.innerHTML = marked(value)
-
-        updateLS()
-    })
-
-    document.body.appendChild(note)
-}
-
-function updateLS() {
-    const notesText = document.querySelectorAll('textarea')
-
-    const notes = []
-
-    notesText.forEach(note => notes.push(note.value))
-
-    localStorage.setItem('notes', JSON.stringify(notes))
-}
+  function updateLS() {
+  const notesText = document.querySelectorAll('textarea');
+  const notes = [];
+  notesText.forEach(note => notes.push(note.value));
+  localStorage.setItem('notes', JSON.stringify(notes));
+  }
+  
+  function addNote(){
+  addNewNote();
+  }
+  
+  
